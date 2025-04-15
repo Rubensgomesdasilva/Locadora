2cc733f03048024f58c876b3dfdd5f03b095bb89
Rubensgomesdasilva <104952992+Rubensgomesdasilva@users.noreply.github.com>
2025-04-15 19:52:05 -0400

    Add files via upload

---
+++ Filme.cs
@@ -0,0 +1,10 @@
+﻿namespace ApiLocadora
+{
+    public class Filme
+    {
+        public Guid id { get; set; } = Guid.NewGuid();
+        public string Nome { get; set; }
+        public string Genero { get; set; }
+        
+    }
+}
